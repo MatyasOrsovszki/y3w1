713e45b69b2fbd333c48147df0231126a9e0c2e5
Iris Adams <117635802+wb21021@users.noreply.github.com>
2024-09-19 11:11:40 +0100

    fixed it for you:)

--- codewitherror.py
+++ codewitherror.py
@@ -1,22 +1,20 @@
-problem = input("Please input the problem in the format a # b where # is the operator and a and b are numbers)
-number 1 = float(problem.split()[0])
-number 2 = float(problem.split()[-1])
+problem = input("Please input the problem in the format a # b where # is the operator and a and b are numbers")
+number1 = float(problem.split()[0])
+number2 = float(problem.split()[-1])
 operation = str(problem.split()[1])
 
 class CALCULATOR:
-    def add(self,a,b):
+    def add(a,b):
         return a+b
-    def subtract(self,a,b):
+    def subtract(a,b):
         return a-b
-    def multiply(self,a,b):
+    def multiply(a,b):
         return a*b
-    def divide(self,a,b):
+    def divide(a,b):
         return a/b
 
-CALC = calculator
+CALC = CALCULATOR()
 
 operations = {"+" : CALC.add(number1,number2), "-" : CALC.subtract(number1,number2), "*" : CALC.multiply(number1,number2), "/": CALC.divide(number1,number2)}
 
 print(operations[operation])
-
-    
